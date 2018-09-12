14f69bea09c6b13e82cafe066d8b5dad84388734
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2018-09-12 20:43:00 +0100

    Install and configure the eslint and pretty

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send({
-    message: "Express application using ES2015 and Babel"
-  });
+app.get('/', (req, res) => {
+	return res.send({
+		message: 'Express application using ES2015 and Babel'
+	});
 });
 
 app
-  .listen(3400)
-  .on("listening", () => {
-    console.log(`The magic happens on port 3400`);
-  })
-  .on("error", error => {
-    console.error(`Something wrong, ${err.message}`);
-  });
+	.listen(3400)
+	.on('listening', () => {
+		console.log('The magic happens on port 3400');
+	})
+	.on('error', error => {
+		console.error(`Something wrong, ${error.message}`);
+	});
